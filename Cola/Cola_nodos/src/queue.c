5d048be46d93ea6bf4aeb5f38c158098a1aaaffd
JuanCarlosSotoGarcia <carloscc1125@gmail.com>
2025-03-20 20:48:24 -0600

    queue.c

--- Cola/Cola_nodos/src/queue.c
+++ Cola/Cola_nodos/src/queue.c
@@ -1,30 +1,71 @@
 #include "queue.h"
 #include <stdlib.h>
 
-Queue* queue_create(){
-
+/**
+ * Crea una nueva cola vacía y la devuelve.
+**/
+Queue* queue_create() {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    if (q == NULL) {
+        return NULL; 
+    }
+    q->head = NULL; 
+    q->tail = NULL; 
+    return q;
 }
 
-void queue_enqueue(Queue* q, Data d){
+void queue_enqueue(Queue* q, Data d) {
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    if (new_node == NULL) {
+        return;
+    }
+    new_node->data = d;
+    new_node->next = NULL;
 
+    if (q->tail != NULL) {
+        q->tail->next = new_node;
+    }
+    q->tail = new_node; 
+
+    if (q->head == NULL) {
+        q->head = new_node; 
+    }
 }
 
-Data queue_dequeue(Queue* q){
+Data queue_dequeue(Queue* q) {
+    if (q->head == NULL) {
+        return -1;
+    }
+    Node* temp = q->head;
+    Data front = temp->data;
+    q->head = q->head->next; 
 
+    if (q->head == NULL) {
+        q->tail = NULL;
+    }
+
+    free(temp); 
+    return front;
 }
 
-bool queue_is_empty(Queue* q){
-
+bool queue_is_empty(Queue* q) {
+    return q->head == NULL; 
 }
 
-Data queue_front(Queue* q){
-
+Data queue_front(Queue* q) {
+    if (q->head == NULL) {
+        return -1; 
+    }
+    return q->head->data;
 }
 
-void queue_empty(Queue* q){
-
+void queue_empty(Queue* q) {
+    while (!queue_is_empty(q)) {
+        queue_dequeue(q);
+    }
 }
 
-void queue_delete(Queue* q){
-
+void queue_delete(Queue* q) {
+    queue_empty(q); 
+    free(q); 
 }
